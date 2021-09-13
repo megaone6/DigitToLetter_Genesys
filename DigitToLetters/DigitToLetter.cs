ea503ba5df41605ac63005ea917a20a00705983e
Dóra László <65315172+megaone6@users.noreply.github.com>
2021-09-13 19:51:44 +0200

    Required tasks completed, no unit testing yet

---
+++ DigitToLetters/DigitToLetter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DigitToLetters
+{
+    public class DigitToLetter
+    {
+        private readonly String digits; // String for the input
+        private IEnumerable<String> combinations; // an enumerable type for the combinations of letters
+
+        private readonly ReadOnlyDictionary<char, List<char>> numbersAndLetters; // a read only Dictionary for storing the number-letter combinations
+
+        public DigitToLetter(String digitsInput) // constructor with parameter
+        {
+            if (digitsInput.Length < 0 || digitsInput.Length > 4 || !digitsInput.All(char.IsDigit) || digitsInput.Contains('1') || digitsInput.Contains('0'))
+            {
+                Console.WriteLine("Input is invalid! Defaulting to empty string...");
+                digits = "";
+            }
+            else
+                digits = digitsInput;
+
+            numbersAndLetters = new ReadOnlyDictionary<char, List<char>>(new Dictionary<char, List<char>>() // initializing the dictionary of number-letter combinations (letters are not in order to prove that the sorting algorithm works)
+            {
+                { '2', new List<char>() { 'b', 'a', 'c' } },
+                { '3', new List<char>() { 'd', 'f', 'e' } },
+                { '4', new List<char>() { 'i', 'h', 'g' } },
+                { '5', new List<char>() { 'k', 'j', 'l' } },
+                { '6', new List<char>() { 'm', 'n', 'o' } },
+                { '7', new List<char>() { 's', 'q', 'r', 'p' } },
+                { '8', new List<char>() { 't', 'v', 'u' } },
+                { '9', new List<char>() { 'w', 'z', 'x', 'y' } },
+            });
+
+            if (digits == "")
+                combinations = new List<String>();
+            else
+                combinations = new List<String> { null };
+        }
+
+        public List<String> GetCombinations() // this function returns the list of combinations (unsorted)
+        {
+            List<List<char>> listOfLists = new List<List<char>>(); // initializing an empty list of list of chars
+
+            foreach (char c in digits) // adding the needed lists to the list of lists based on their keys in the dictionary
+            {
+                listOfLists.Add(numbersAndLetters[c]);
+            }
+            foreach (List<char> list in listOfLists) // joining the current result with each element of the next list
+            {
+                combinations = combinations.SelectMany(x => list.Select(y => x + y));
+            }
+
+            return combinations.ToList();
+        }
+
+        public List<String> GetCombinationsSorted() // this function gets the return value of the getCombinations function and sorts it with the insertion sort algorithm, returning the sorted list
+        {
+            List<String> combinationList = GetCombinations();
+            int i, j;
+
+            for (i = 1; i < combinationList.Count; i++)
+            {
+                String current = combinationList[i];
+                j = i - 1;
+                while ((j >= 0) && (combinationList[j].CompareTo(current) > 0))
+                {
+                    combinationList[j + 1] = combinationList[j];
+                    j--;
+                }
+                combinationList[j + 1] = current;
+            }
+
+            return combinationList;
+        }
+    }
+}
---
+++ DigitToLetters/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DigitToLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String digits;
+            do // if the input is invalid, the loop continues and asks for the input again
+            {
+                Console.WriteLine("What is your input? Constraints: length of 0-4, only contains digits from 2-9");
+                digits = Console.ReadLine();
+                if (digits.Length < 0 || digits.Length > 4 || !digits.All(char.IsDigit) || digits.Contains('1') || digits.Contains('0'))
+                    Console.WriteLine("Input does not meet the requirements! (length of 0-4, only contains digits from 2-9)\n");
+            } while (digits.Length < 0 || digits.Length > 4 || !digits.All(char.IsDigit) || digits.Contains('1') || digits.Contains('0'));
+
+            DigitToLetter dtl1 = new DigitToLetter(digits);
+            //List<String> combinations1 = dtl1.GetCombinations();
+            List<String> combinations1 = dtl1.GetCombinationsSorted();
+            Console.WriteLine("Number of combinations: " + combinations1.Count);
+            combinations1.ForEach(Console.WriteLine);
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            DigitToLetter dtl2 = new DigitToLetter("3974");
+            //List<String> combinations2 = dtl2.GetCombinations();
+            List<String> combinations2 = dtl2.GetCombinationsSorted();
+            Console.WriteLine("Number of combinations: " + combinations2.Count);
+            combinations2.ForEach(Console.WriteLine);
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            DigitToLetter dtl3 = new DigitToLetter("184a9e");
+            //List<String> combinations3 = dtl3.GetCombinations();
+            List<String> combinations3 = dtl3.GetCombinationsSorted();
+            Console.WriteLine("Number of combinations: " + combinations3.Count);
+            combinations3.ForEach(Console.WriteLine);
+
+            Console.WriteLine("Press any key to quit the program!");
+            Console.ReadKey();
+        }
+    }
+}
